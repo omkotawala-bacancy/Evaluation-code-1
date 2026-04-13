e1e6f8905667ca3f3cffebb553640abd19d063af
omkotawala-bacancy <om.kotawala@bacancy.us>
2026-04-13 15:36:30 +0530

    Change

---
+++ evaluation-1/src/component/Tag.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Tag({tag}) {
+
+    function handleClick(){
+        
+    }
+  return (
+    <div>
+        <h1>tag.title</h1>
+        <button onClick={handleClick}></button>
+    </div>
+  )
+}
+
+export default Tag
